9297ebf4236dee24bc81f2b64fe424ba6af13212
DmytroPetrenko <44116687+DmytroPetrenko@users.noreply.github.com>
2021-10-07 17:51:44 +0300

    basic form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,13 @@
-<template></template>
+<template>
+	<div id="home">
+		<the-verification-stepper />
+	</div>
+</template>
 
 <script>
+import TheVerificationStepper from "@/components/TheVerificationStepper"
 export default {
 	name: "Home",
+	components: { TheVerificationStepper },
 }
 </script>
